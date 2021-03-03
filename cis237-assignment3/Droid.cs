9551e3dca1b68fd98945d9c0d5cf2472c8dc156e
Skylar Peters <skylarpeters@ymail.com>
2021-03-03 07:06:22 -0500

    Work on Droid Class.

--- cis237-assignment3/Droid.cs
+++ cis237-assignment3/Droid.cs
@@ -12,6 +12,48 @@ namespace cis237_assignment3
 {
     abstract class Droid : IDroid
     {
+        //*****************************
+        // Variables
+        //*****************************
+        protected string material;
+        protected string color;
+        protected decimal totalCost;
 
+        //*****************************
+        // Properties
+        //*****************************
+        public string Material
+        {
+            get { return material; }
+            set { material = value; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public abstract decimal TotalCost { get; set; }
+
+        //******************************
+        // Public Methods
+        //******************************
+        // ToString method to return a formatted string containing the variables
+        public override string ToString()
+        {
+            return $"{material} {color} {totalCost}";
+        }
+
+        public abstract void CalculateTotalCost();
+
+        //*****************************
+        // Constructors
+        //*****************************
+        public Droid(string Material, string Color)
+        {
+            this.material = Material;
+            this.color = Color;
+        }
     }
 }
